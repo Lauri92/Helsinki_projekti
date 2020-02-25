1ee553f9c97beccab5ba0491814f84375347ab9c
Tuomas Sihvo <tuomas.sihvo@metropolia.fi>
2020-02-25 14:49:18 +0200

    ohje overlay WIP

---
+++ Js/Overlay_ohjeet.js
@@ -0,0 +1,21 @@
+'use strict';
+
+function on() {
+    document.getElementById("overlay").style.display = "block";
+    body.className = "overlay_hidden";
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+    body.className = "overlay_show";
+}
+
+function infocreate() {
+
+}
+//  Start of the script
+const body = document.getElementsByTagName("body")[0];
+const nappi7 = document.getElementById("nappi7");
+const overlay = document.getElementById("overlay");
+nappi7.addEventListener("click", on);
+overlay.addEventListener("click", off);
